ecbdf58e2ab99583591b0735c2718a81b197b861
kgignatyev <kgignatyev@gmail.com>
2019-05-10 22:50:48 -0700

    wip;

--- helpers/set-sample-data.sh
+++ helpers/set-sample-data.sh
@@ -4,5 +4,6 @@ consul kv put default/count 1
 consul kv put default/pgdb/url "postgresql://localhost/db"
 consul kv put us-west-1/pgdb/url "postgresql://us-west-1/db"
 consul kv put us-east-2/pgdb/url "postgresql://east/db"
+consul kv put us-east-2/myapp/pgdb/url "postgresql://east-myapp/db"
 consul kv put myapp/pgdb/url "postgresql://localhost/mydb"
-consul kv put myapp/release1/pgdb/url "postgresql://localhost/mydb-r1"
+consul kv put default/myapp/release1/pgdb/url "postgresql://localhost/mydb-r1"
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
 extern crate reqwest;
 extern crate serde_json;
+extern crate base64;
 
-use std::io::Read;
 
-use std::io::{Error,ErrorKind};
+use std::io::{Read, Error};
 use std::env;
+use serde_json::{ Value};
 
 
 
-fn get_key_value(path: &String) -> Result<String,Error> {
+
+enum CustomError {
+    Io(std::io::Error),
+    Reqwest(reqwest::Error),
+    Info(String),
+}
+
+impl From<std::io::Error> for CustomError {
+    fn from(err: std::io::Error) -> CustomError {
+        CustomError::Io(err)
+    }
+}
+
+impl From<reqwest::Error> for CustomError {
+    fn from(err: reqwest::Error) -> CustomError {
+        CustomError::Reqwest(err)
+    }
+}
+
+impl From<String> for CustomError {
+    fn from(err: String) -> CustomError {
+        CustomError::Info(err)
+    }
+}
+
+
+fn get_key_value(path: &String) -> Result<String, CustomError> {
 //    let mut res = reqwest::get("http://httpbin.org/get")?;
-    let url = format!( "http://localhost:8500/v1/kv/{}", path);
-    println!("trying:{}",url);
+    let url = format!("http://localhost:8500/v1/kv/{}", path);
+    println!("trying:{}", url);
 
     let mut res = reqwest::get(&url)?;
     let mut body = String::new();
@@ -23,22 +50,28 @@ fn get_key_value(path: &String) -> Result<String,Error> {
     if res.status() == 200 {
         Ok(body)
     } else {
-        Err(Error::new( ErrorKind::NotFound ,""))
+        Err(CustomError::Info("no value".to_string()))
     }
 }
 
 
-fn find_key_value(key: &String, path_parts: &[String]) {
+fn find_key_value(key: &String, path_parts: &[String])->Option<String> {
     let mut parts = path_parts.to_vec();
     parts.reverse();
-    let path = format!( "{}/{}", parts.join("/") , key);
-    let r = get_key_value( &path );
-    match r {
-        Ok(x)  => println!("OK: got - {}",x),
-        Err(_) => println!("Error")
+    let mut res: Option<String> = None;
+    let length = parts.len();
+    for x in 0..length {
+        if res.is_none() {
+            let (head, _tail) = parts.split_at(length - x);
+            let path = format!("{}/{}", head.join("/"), key);
+            let r = get_key_value(&path);
+            match r {
+                Ok(x) => {println!("OK: got - {}", x);res = Some(x);},
+                Err(_) => println!("Error")
+            }
+        }
     }
-
-    
+    res
 }
 
 fn main() {
@@ -46,6 +79,27 @@ fn main() {
     let (left, path_parts) = args.split_at(2);
     let key = left.get(1).unwrap();
     let res = find_key_value(&key, &path_parts);
+    match res {
+        Some(data) => {
+            let r:Result<Value,serde_json::Error> = serde_json::from_str(&data);
+            match r {
+                Ok(v) => {
+                    let b64v = v[0]["Value"].to_string();
+                    let vd =  base64::decode( &b64v).unwrap();
+
+                    let s = String::from_raw_parts(vd).unwrap();
+
+
+                    println!("OK: got key - {} = {} = {}",v[0]["Key"],b64v, s);
+                },
+                Err(err) => {}
+            }
+        },
+        None =>{
+
+        }
+    }
+
     println!("{:?}", args);
 //    run();
 }
